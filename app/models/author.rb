89828ee9c85a942d658747772b86186ed791ccb5
Adam <github email address>
2018-09-11 16:36:52 -0600

    stubbed methods for models

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,10 @@
 class Author < ActiveRecord::Base
   has_many :books
   has_many :users, through: :books
+
+  #find the most common genre from the author's books and return it
+  #ties decided by newest? or oldest?
+  def most_popular_genre
+    return "genre"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_many :books
   has_many :authors, through: :books
   has_secure_password
+
+  #return the most common author
+  #ties return the newest? or oldest?
+  def favorite_author
+    return "activerecord of favorite author"
+  end
 end
