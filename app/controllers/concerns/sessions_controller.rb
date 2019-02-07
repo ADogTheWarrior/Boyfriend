ce79a5775bfc0e8ca55ff37e2ad6fb489aa622b9
Adam <github email address>
2019-02-07 11:38:08 -0700

    forgot to do a commit for a while, looks like a lot of validation implimentation for models

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -9,7 +9,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    raise request.env["omniauth.auth"].to_yaml
+    # request is an object i can look at in pry
+    # raise request.env["omniauth.auth"].to_yaml
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+# binding.pry
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+    end
+
+    session[:user_id] = @user.id
+
+    render 'users/index'
   end
 
   def edit
@@ -24,8 +34,30 @@ class SessionsController < ApplicationController
 private
 
   def auth
+    request.env['omniauth.auth']
     #request env omniauth
     #  raise request.env["omniauth.auth"].to_yaml
     #private method
   end
 end
+
+
+# class SessionsController < ApplicationController
+#   def create
+#     @user = User.find_or_create_by(uid: auth['uid']) do |u|
+#       u.name = auth['info']['name']
+#       u.email = auth['info']['email']
+#       u.image = auth['info']['image']
+#     end
+#
+#     session[:user_id] = @user.id
+#
+#     render 'welcome/home'
+#   end
+#
+#   private
+#
+#   def auth
+#     request.env['omniauth.auth']
+#   end
+# end
--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[4.2]
     create_table :users do |t|
       t.string :provider
       t.string :uid
-        t.string :name
+      t.string :name
       t.string :email
 
       t.timestamps null: false
