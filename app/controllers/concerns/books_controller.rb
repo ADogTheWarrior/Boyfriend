38d34576cae3c5ede5f0f3fa69a86127a72f7268
Adam <github email address>
2019-03-21 14:40:56 -0600

    added nested route of book index through author and started messing with a book partial

--- app/controllers/concerns/books_controller.rb
+++ app/controllers/concerns/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    if params[:author_id]
+      @books= Author.find(params[:author_id]).books
+    else
+      @books = Book.all
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :books
-  resources :authors
+  resources :authors do
+    resources :books, only: [:show, :index]
+  end
   resources :sessions
   get '/auth/facebook/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy', :as => :signout
