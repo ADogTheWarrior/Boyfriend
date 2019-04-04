0d5c5eb57389689549ffa97ac39228de8f50718d
Adam <github email address>
2019-04-04 16:16:14 -0600

    bugs found and fixed in the genre enum for book model

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 4) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "genre"
     t.integer "pages"
     t.integer "author_id"
+    t.integer "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
