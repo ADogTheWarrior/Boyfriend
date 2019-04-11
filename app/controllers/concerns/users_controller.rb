e5e0cc0f8056bfd7ddeb97393b44ecb8232e3b2c
Adam <github email address>
2019-04-11 10:31:34 -0600

    bug fixing for the user password

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def require_login
