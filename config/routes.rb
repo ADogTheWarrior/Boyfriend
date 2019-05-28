ed857cccf7c7a0e2b210571cc7739bc2c1700b4a
Adam <github email address>
2019-05-28 10:09:11 -0600

    notes made in routes for futre paths, user view updated

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   end
   resources :sessions
   get '/auth/facebook/callback' => 'sessions#create'
-  get '/signout' => 'sessions#destroy', :as => :signout
   get '/signin' => 'sessions#new', :as => :signin
+  # get '/signin' => 'users#new'
+  get '/signout' => 'sessions#destroy', :as => :signout
+  # get '/signout' => 'users#destroy'
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 end
