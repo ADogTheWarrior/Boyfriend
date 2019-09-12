58378ed977b3608c4822e87c5baa830e07256dda
Adam <github email address>
2019-09-12 16:41:18 -0600

    cleaning up notes from files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,11 @@ class User < ActiveRecord::Base
   has_many :authors, through: :books
   has_secure_password
 
-  # validates :provider Just not sure how best to validate right now
-  # validates :uid      Just not sure how best to validate right now
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  #return the most common author
-  #ties return the newest? or oldest?
+  #returns the User with the biggest bookshelf
   def self.largest_bookshelf
-    # binding.pry
     User.left_joins(:books).group('books.id').order('count(books.id) desc').limit(1).first
   end
 end
