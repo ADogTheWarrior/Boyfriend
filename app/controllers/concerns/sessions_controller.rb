1bbb11c57b5fe6f9f3c91c65afcfa291a89b8f11
Adam <github email address>
2019-09-18 11:55:53 -0600

    got the login working, just need to incorperate the omniauth part

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -22,7 +22,15 @@ class SessionsController < ApplicationController
   # end
 
   def create
-    
+    @user = User.find_by(email: params[:email])
+      # binding.pry
+
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to '/'
+    end
   end
 
   def destroy
